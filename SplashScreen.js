abdc08fef57cedf38916e86a6af59bd6983017ac
p-rajan178310 <p_rajan178310@fanshaweonline.ca>
2023-06-26 02:09:57 -0400

    Initial project code

---
+++ SplashScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+export default class SplashScreenComponent extends React.Component {
+  componentDidMount() {
+    SplashScreen.preventAutoHideAsync();
+    // Simulate a background task or fetch data
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 3000); // Replace 3000 with the desired delay for your splash screen
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/splash.png')}
+          style={styles.splashImage}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splashImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
